ebf569549c873127b0806af1fe8a6d0ef48145f7
Paul Guirguis <paul.guirguis@my.jcu.edu.au>
2024-11-15 19:14:12 +1000

    test movement script

---
+++ Scripts/dummy_sprite.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+
+# Movement speed
+var speed = 200 
+
+# Called every frame
+func _process(delta):
+	#Horizontal movement
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	elif Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+		
+	#Vertical movement
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
+	elif Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
